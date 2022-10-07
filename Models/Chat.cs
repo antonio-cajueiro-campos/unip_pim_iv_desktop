7468283cda80c1a01160de7d9e33f13cbdb4e18f
antonio-cajueiro-campos <kimeesan5@gmail.com>
2022-10-06 22:26:25 -0300

    chat service

---
+++ Models/Chat.cs
@@ -0,0 +1,9 @@
+﻿namespace unip_pim_iv_desktop.Models;
+
+internal class Chat
+{
+    public long ChatId { get; set; }
+    public string Type { get; set; }
+    public UserHub[] UserList { get; set; }
+    public Message[] MessagesList { get; set; }
+}
---
+++ Models/UserHub.cs
@@ -0,0 +1,9 @@
+﻿namespace unip_pim_iv_desktop.Models;
+
+internal class UserHub
+{
+    public long UserId { get; set; }
+	public string UserConnectionId { get; set; }
+    public string Username { get; set; }
+    public string Role { get; set; }
+}
